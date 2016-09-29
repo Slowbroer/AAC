37a9c1fe591cbce714fcae691a9f08b55e641a40
slowbroer <1573975217@qq.com>
2016-09-29 14:25:37 +0800

    1.添加博客列表页

---
+++ frontend/views/blog/list.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linpeiyu
+ * Date: 2016-09-27
+ * Time: 15:14
+ */
+?>
+
+
+<ul>
+    <?php foreach($lists as $list){ ?>
+    <li><a href="<?= \yii\helpers\Url::toRoute('blog/info/id/'.$list['id']) ?>"><?= $list['title'] ?></a></li>
+    <?php } ?>
+</ul>
+
+
+
+
