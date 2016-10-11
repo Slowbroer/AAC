6ff9f686c894d774dd46d2eb62e7c206d2171294
slowbroer <1573975217@qq.com>
2016-10-11 09:39:27 +0800

    1.调试

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -49,9 +49,8 @@ class UserController extends Controller {
     }
 
     public function actionRecent_blog(){
-
-        var_dump(Yii::$app->user);
         $user_id = Yii::$app->user->id;
+        var_dump(Yii::$app->user);
 //        $user_id = isset($_POST['user_id'])? $_POST['user_id']:0;
         $blog = new Blog();
 
