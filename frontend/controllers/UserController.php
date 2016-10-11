beaa282447a3e11ff3f627c780578d4130b4ebc9
slowbroer <1573975217@qq.com>
2016-10-11 09:57:04 +0800

    1.调试

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -50,7 +50,7 @@ class UserController extends Controller {
 
     public function actionRecent_blog(){
 //        var_dump(Yii::$app->user);
-//        $user_id = Yii::$app->user->identity->id;
+        $user_id = Yii::$app->user->identity->id;
 //        sleep(5);
 
 //        if(isset(Yii::$app->user->identity))
@@ -62,8 +62,8 @@ class UserController extends Controller {
 //            return "can not get the user_id ";
 //        }
 
-        isset(Yii::$app->user->identity->id);
-        $user_id = isset($_POST['user_id'])? $_POST['user_id']:0;
+        var_dump(Yii::$app->user->identity->id);
+//        $user_id = isset($_POST['user_id'])? $_POST['user_id']:0;
         $blog = new Blog();
 
         $list = $blog->getRecent($user_id);
